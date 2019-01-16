6f3d720e7a34ce9f73327b11c43ca45299092d19
Victor Andersson <victor.andersson3@stud.pitea.se>
2019-01-16 14:04:57 +0100

    functions
    Fixar lite enkla funktioner som man behöver för att få allt å fungera

---
+++ func.py
@@ -0,0 +1,20 @@
+def change_name():
+    pass
+
+def save():
+    pass
+
+def load_save():
+    pass
+
+def move_forward():
+    pass
+
+def move_left():
+    pass
+
+def move_right():
+    pass
+
+def move_back():
+    pass
