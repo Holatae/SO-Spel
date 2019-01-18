904287f356b7c8aa8e63d4088048a7a21aa12b89
Victor Andersson <victor.andersson3@stud.pitea.se>
2019-01-18 14:05:51 +0100

    Update player.py

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ health = 0
 money = 0
 power = 0
 speed = 0
+defense_power = 0
 
 def attack(power_of_enemy, speed_of_enemy, health_of_enemy):
     pass
@@ -11,14 +12,27 @@ def attack(power_of_enemy, speed_of_enemy, health_of_enemy):
 def get_attacked(power_of_enemy, speed_of_enemy, health_of_enemy):
     pass
 
-def run():
-    pass
+def block(power_of_enemy, speed_of_enemy):
+    if power_of_enemy > defense_power:
+        get_attacked(power_of_enemy - defense_power, 0, 0)
+
+def run(speed_of_enemy):
+    if speed > speed_of_enemy:
+        return True
+    else:
+        return False
 
 def war():
     pass
 
 def pay(amount):
-    pass
+    global money
+    if amount > money:
+        return False
+    else:
+        money = money - amount
+        return True
 
 def get_payed(amount):
-    pass
+    global money
+    money = money + amount
